aa4445cfbbc93eae52128ff5d91d3b45590a75bc
Mario Pardo <mario.pardo003@gmail.com>
2026-01-07 16:13:01 -0500

    enabled lidar

--- webots/controllers/TurtleBotGoForward/TurtleBotGoForward.py
+++ webots/controllers/TurtleBotGoForward/TurtleBotGoForward.py
@@ -12,22 +12,22 @@ robot = Robot()
 keyboard = robot.getKeyboard()
 keyboard.enable(TIME_STEP)
 
-
+#Drive Setup
 leftMotor = robot.getDevice("left wheel motor")
 rightMotor = robot.getDevice("right wheel motor")
-
 leftMotor.setPosition(float('inf'))
 rightMotor.setPosition(float('inf'))
 
 
+#Lidar Setup
+lidar = robot.getLidar("lidar")
+lidar.enable(TIME_STEP)
+
+
 while robot.step(TIME_STEP) != -1:
+    
     key = keyboard.getKey()
-
-    # get the time step of the current world.
     timestep = int(robot.getBasicTimeStep())
-
-    leftMotor = robot.getDevice("left wheel motor")
-    rightMotor = robot.getDevice("right wheel motor")
 
     linear_speed = 0.0
     angular_speed = 0.0
@@ -35,14 +35,22 @@ while robot.step(TIME_STEP) != -1:
     if key == ord('W') or key == ord('w'):
         linear_speed = MAX_SPEED
     elif key == ord('S') or key == ord('s'):
-        linear_speed = -MAX_SPEED
+        linear_speed = -MAX_SPEED 
 
     if key == ord('A') or key == ord('a'):
-        angular_speed = MAX_SPEED
+        angular_speed = MAX_SPEED 
     elif key == ord('D') or key == ord('d'):
-        angular_speed = -MAX_SPEED
+        angular_speed = -MAX_SPEED 
 
     leftMotor.setVelocity(linear_speed - angular_speed)
     rightMotor.setVelocity(linear_speed + angular_speed)
+
+    #Lidar
+
+    range_image = lidar.getRangeImage()
+    lidar.enablePointCloud()
+    
+
+
 
     
